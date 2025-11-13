19b3b9df69740d12dd8c23882213da1ddc7fb233
paul schmidt <schmidt@heitaria.ch>
2025-11-13 09:05:55 +0100

    chore: push pending changes

--- server.js
+++ server.js
@@ -3059,15 +3059,8 @@ function authGuard(req, res, next) {
   }
 }
 
-// --- auth guard (simple) ---
-const authGuard = (req, res, next) => {
-  if (req?.user?.address) return next();
-  return res.status(401).json({ error: 'Unauthorized' });
-};
-
 // If some routes still use `requireAuth`, keep this alias:
 const requireAuth = authGuard;
-
 
 // ========================================================================
 
