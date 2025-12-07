bbc21e3a3e8e7fd1f4b4f5f7aafcc5b95d57fdaf
paul schmidt <schmidt@heitaria.ch>
2025-12-07 14:24:17 +0100

    chore: push pending changes

--- server.js
+++ server.js
@@ -6064,6 +6064,7 @@ app.get('/admin/anchor', async (req, res) => {
     } else {
       tenantsToAnchor = [req.tenantId];
     }
+    console.log(`[Anchor] Tenants to process: ${tenantsToAnchor.join(', ')}`);
 
     const results = [];
     for (const tId of tenantsToAnchor) {
