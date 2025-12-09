db14a39863ecf482aa60f9c318754dc0f313f97c
paul schmidt <schmidt@heitaria.ch>
2025-12-09 19:57:51 +0100

    chore: push pending changes

--- server.js
+++ server.js
@@ -336,6 +336,7 @@ const CORS_ORIGINS = (process.env.CORS_ORIGINS || process.env.CORS_ORIGIN || DEF
   .filter(Boolean);
 
 CORS_ORIGINS.push("https://sally-uyuni-855007491806.us-west1.run.app");
+CORS_ORIGINS.push("https://vendor-842701335150.us-west1.run.app");
 
 const PINATA_API_KEY = (process.env.PINATA_API_KEY || "").trim();
 const PINATA_SECRET_API_KEY = (process.env.PINATA_SECRET_API_KEY || "").trim();
